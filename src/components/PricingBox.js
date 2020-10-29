991184bab013c8cea87714011dcd2a17a7403559
juliaNYC <siwak.julia@yahoo.com>
2020-10-29 12:24:46 +0200

    add css

--- src/components/PricingBox.js
+++ src/components/PricingBox.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FaAngleDoubleDown, FaAngleDoubleUp } from "react-icons/fa";
 
-const PricingBox = ({id, level, CTAText, featuresList}) => {
-  const renderFeaturesList = featuresList.map((feature, index) => <li key={id+index} className="feature-item">{feature}</li>)
+const PricingBox = ({id, level, CTAText, featuresList, featuresToShow}) => {
+    const [readMore, setReadMore] = useState(false);
+
+    const renderFeaturesList = (readMore ? featuresList
+        : featuresList.slice(0, featuresToShow)).map((feature, index) => <li key={id + index} className="feature-item">{feature}</li>);
+
+    const showMoreFeatures = () => {
+        if (featuresList.length > featuresToShow) {
+            let span;
+            if (readMore) span = <span>Show less features <FaAngleDoubleUp /></span>;
+            else span = <span>Show all features <FaAngleDoubleDown /></span>;
+            return <button className="read-more-button" onClick={() => setReadMore(!readMore)}>{span}</button>;
+        }
+    };
+
     return (
         <div className="pricing-box">
             <h1 className="level">{level}</h1>
             <button className="cta-text-button">{CTAText}</button>
             <ul className="features-list">{renderFeaturesList}</ul>
+            {showMoreFeatures()}
         </div>
     )
 };
 
 export default PricingBox;
-
